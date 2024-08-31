6b94ef8e23b5471a823526e3751c555b1bdba241
SUDIP KUNDU <sudip2003kundu@gmail.com>
2024-08-31 13:58:15 +0530

    schema.gql readonly file path changed to relative path

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,5 +1,4 @@
-
-import { Module, MiddlewareConsumer } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
@@ -11,20 +10,18 @@ import { MusicModule } from './music/music.module';
 import { SignupModule } from './signup/signup.module';
 import { MyLoggerModule } from './my-logger/my-logger.module';
 import { MiddlewareModule } from './middleware/middleware.module';
-import { MiddlewareService } from './middleware/middleware.service';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       playground: true,
-      autoSchemaFile: join(process.cwd(), 'schema.gql'),
+      autoSchemaFile: process.env.NODE_ENV === 'production' ? false : join(process.cwd(), 'schema.gql'),
       context: ({ req }) => ({ req }),
     }),
     DatabaseModule,
     MusicModule,
     SignupModule,
-
     MyLoggerModule,
     MiddlewareModule,
   ],
@@ -32,8 +29,6 @@ import { MiddlewareService } from './middleware/middleware.service';
   providers: [
     AppService,
     AppResolver,
-    
   ],
 })
-export class AppModule {
-}
+export class AppModule {}
