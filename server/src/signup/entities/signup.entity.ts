e6d6838916b18c7658cd263dd6c9f3b1ec8c2b6a
SUDIP KUNDU <sudip2003kundu@gmail.com>
2024-09-08 16:15:22 +0530

    rxjs/operators production issue

--- server/src/signup/entities/signup.entity.ts
+++ server/src/signup/entities/signup.entity.ts
@@ -20,6 +20,8 @@ export class SignupResponse {
   message: string;
 }
 
+
+@ObjectType()
 export class LastHistory {
   @Field()
   musicUrl: string;
--- server/src/signup/signup.resolver.ts
+++ server/src/signup/signup.resolver.ts
@@ -2,17 +2,22 @@ import { Resolver, Mutation, Args, Query, Context } from '@nestjs/graphql';
 import { SignupService } from './signup.service';
 import { CreateSignupInput } from './dto/create-signup.input';
 
-import { Signup, SignupResponse,LastHistory } from './entities/signup.entity';
-import { UseGuards } from '@nestjs/common';
+import { LastHistory, Signup,SignupResponse } from './entities/signup.entity';
 import { AuthGuard } from '../authguard/authguard.service';
-
+import { UseGuards } from '@nestjs/common';
 
 @Resolver(() => SignupResponse)
 export class SignupResolver {
-  constructor(private readonly signupService: SignupService) { }
-  @Query(returns => [LastHistory])
+  constructor(private readonly signupService: SignupService) {}
+
+  @Mutation(() => SignupResponse)
+  async create(@Args('createSignupInput') createSignupInput: CreateSignupInput): Promise<SignupResponse> {
+    return this.signupService.create(createSignupInput);
+  }
+
+  @Query(returns => LastHistory)
   @UseGuards(AuthGuard)
-  async getMusicByUserId(@Context() context): Promise<LastHistory | null> {
+  async getLastHistory(@Context() context): Promise<LastHistory | null> {
     const userId = context.req['firebaseUserId'];
     if (userId === "null" || userId === "invalid") {
       return null;
